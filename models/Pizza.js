3fbc3e7e99d7f64f8b91a4631b33e301ad203b0d
Anastasia Perez <Stasiperez85@gmail.com>
2022-04-11 16:57:35 -0600

    update dates and add comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,3 +1,5 @@
+const dateFormat = require('../utils/dateFormat');
+
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema(
@@ -11,6 +13,7 @@ const PizzaSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
         },
         size: {
             type: String,
@@ -27,6 +30,7 @@ const PizzaSchema = new Schema(
     {
         toJSON: {
             virtuals: true,
+            getters: true
         },
         id: false
     }
