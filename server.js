9da548266e462fc40e048d34f8a3ab55d07b7abd
Anastasia Perez <Stasiperez85@gmail.com>
2022-04-13 16:50:31 -0600

    add indexdb

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const mongoose = require('mongoose');
-
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,9 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -19,4 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
+app.use(require('./routes'));
+
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
